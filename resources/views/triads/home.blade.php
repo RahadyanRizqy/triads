97b7ffb7067dd331df573657ff9a21d5f380e5bb
RahadyanRizqy <rahadyanrizqy@gmail.com>
2023-11-05 08:25:54 +0700

    fixing the lang buttons

--- resources/views/triads/home.blade.php
+++ resources/views/triads/home.blade.php
@@ -59,6 +59,53 @@
     a {
         color: #03a9f4;
     }
+
+    .language-switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+}
+
+.language-switch input {
+    display: none;
+}
+
+.language-switch label {
+    background-color: #ccc;
+    border-radius: 34px;
+    display: block;
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    transition: 0.4s;
+    width: 60px;
+    height: 34px;
+}
+
+.language-switch label:after {
+    content: "";
+    border-radius: 50%;
+    background-color: #fff;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    transition: 0.4s;
+    width: 26px;
+    height: 26px;
+}
+
+.language-switch input:checked + label {
+    background-color: #2196F3;
+}
+
+.language-switch input:checked + label:after {
+    left: calc(100% - 26px);
+    transform: translate(-50%, -50%);
+}
+
 </style>
 @endpush
 
@@ -75,9 +122,13 @@
         <div class="id-prologue d-none">
             {!! $res_id !!}
         </div>
-        <div class="d-flex justify-content-center">
-            <button class="btn btn-primary" id="id-btn">ID</button>
-            <button class="btn btn-danger d-none" id="en-btn">EN</button>
+        <div class="d-flex justify-content-center flex-row">
+            <div class="button">
+                <button class="btn btn-primary mx-1 opacity-50" id="id-btn">ID</button>
+            </div>
+            <div class="button">
+                <button class="btn btn-danger mx-1" id="en-btn">EN</button>
+            </div>
         </div>
     </div>
     <p class="text-center medium">owner: <a class="text-decoration-none" href="https://instagram.com/rdn_rzq">@rdn_rzq</a></p>
@@ -86,18 +137,37 @@
 @push('scripts')
 <script>
     $(document).ready(function() {
-        $(document).on('click', '#id-btn', function () {
-            $('#id-btn').addClass('d-none');
-            $('#en-btn').removeClass('d-none');
+        let activeButton = localStorage.getItem('activeButton');
+
+        if (activeButton === 'id') {
+            $('#id-btn').removeClass('opacity-50');
+            $('#en-btn').addClass('opacity-50');
             $('.en-prologue').addClass('d-none');
-            $('.id-prologue').removeClass('d-none')
+            $('.id-prologue').removeClass('d-none');
+            console.log('indonesian');
+        }
+        else {
+            $('#en-btn').removeClass('opacity-50');
+            $('#id-btn').addClass('opacity-50');
+            $('.en-prologue').removeClass('d-none');
+            $('.id-prologue').addClass('d-none');
+            console.log('english');
+        }
+
+        $(document).on('click', '#id-btn', function () {
+            $('#id-btn').removeClass('opacity-50');
+            $('#en-btn').addClass('opacity-50');
+            $('.en-prologue').addClass('d-none');
+            $('.id-prologue').removeClass('d-none');
+            localStorage.setItem('activeButton', 'id');
         });
 
         $(document).on('click', '#en-btn', function () {
-            $('#en-btn').addClass('d-none');
-            $('#id-btn').removeClass('d-none');
-            $('.en-prologue').removeClass('d-none')
+            $('#en-btn').removeClass('opacity-50');
+            $('#id-btn').addClass('opacity-50');
+            $('.en-prologue').removeClass('d-none');
             $('.id-prologue').addClass('d-none');
+            localStorage.setItem('activeButton', 'en');
         });
     });
 </script>
